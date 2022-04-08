aae429ccc0ef6ddf1f98b56a11059a7b52478ed7
Damir <d.xasanshin@bk.ru>
2022-04-08 12:29:16 +0300

    Добавил разметку в index.php

---
+++ php/index.php
@@ -0,0 +1,203 @@
+<?php ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://fonts.googleapis.com/css2?family=PT+Serif:wght@400;700&family=Roboto:wght@400;500&display=swap"
+          rel="stylesheet">
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/fonts.css">
+    <link rel="stylesheet" href="css/jquery.fancybox.min.css">
+    <link rel="stylesheet" href="css/slick.css">
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+
+<body>
+<header class="header">
+    <div class="header__top">
+        <div class="container">
+            <div class="header__contacts">
+                <a class="header__phone" href="tel:89519714913">8 (951) 971 49 13</a>
+                <a class="header__email" href="#">bankkzn@gmail.com</a>
+                <a data-fancybox data-src="#modal" href="javascript:;" class="header__btn" href="#">Бесплатная
+                    консультация</a>
+            </div>
+        </div>
+    </div>
+    <div class="header__content">
+        <div class="container">
+            <div class="header__content-inner">
+                <div class="header__logo">
+                    <a href="#">
+                        <img height="40px" src="img/logo.png" alt="">
+                    </a>
+                </div>
+                <nav class="menu">
+                    <ul>
+                        <li><a href="#">Главная</a></li>
+                        <li><a href="#">О компании</a></li>
+                        <li><a href="#">Услуги</a></li>
+                        <li><a href="#">Контакты</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </div>
+</header>
+<section class="slider">
+    <div class="container">
+        <div class="slider__inner">
+            <div class="slider__item">
+                <div class="slider__item-content">
+                    <div class="slider__title">
+                        КОМПЛЕКСНОЕ ЗАКРЫТИЕ ПРЕДПРИЯТИЯ
+                    </div>
+                    <div class="slider__text">
+                        Полное прекращение существования юридического лица с сохранением легальности всей предыдущей
+                        деятельности.
+                    </div>
+                    <a data-fancybox data-src="#modal" href="javascript:;" class="slider__btn default-btn">
+                        Оставить заявку
+                    </a>
+                </div>
+            </div>
+            <div class="slider__item">
+                <div class="slider__item-content">
+                    <div class="slider__title">
+                        КОМПЛЕКСНОЕ ЗАКРЫТИЕ ПРЕДПРИЯТИЯ
+                    </div>
+                    <div class="slider__text">
+                        Полное прекращение существования юридического лица с сохранением легальности всей предыдущей
+                        деятельности.
+                    </div>
+                    <a data-fancybox data-src="#modal" href="javascript:;" class="slider__btn default-btn">
+                        Оставить заявку
+                    </a>
+                </div>
+            </div>
+            <div class="slider__item">
+                <div class="slider__item-content">
+                    <div class="slider__title">
+                        КОМПЛЕКСНОЕ ЗАКРЫТИЕ ПРЕДПРИЯТИЯ
+                    </div>
+                    <div class="slider__text">
+                        Полное прекращение существования юридического лица с сохранением легальности всей предыдущей
+                        деятельности.
+                    </div>
+                    <a data-fancybox data-src="#modal" href="javascript:;" class="slider__btn default-btn">
+                        Оставить заявку
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="services">
+    <div class="container">
+        <div class="services__top">
+            <div class="services__title-box">
+                <div class="services-title">
+                    Наши услуги
+                </div>
+                <div class="services__text">
+                    Комплексный подход к вашему вопросу, своевременная правовая помощь, представление интересов во
+                    всех судебных инстанциях.
+                </div>
+            </div>
+            <div class="services__btn">
+                <a href="#">
+                    Показать все услуги
+                </a>
+            </div>
+        </div>
+        <div class="services__items">
+            <div class="services__item">
+                <img src="img/about-1.png" alt="">
+                <div class="services__item-title">
+                    Банкротство и ликвидация
+                </div>
+                <div class="services__item-text">
+                    Полное прекращение существования юридического лица с сохранением легальности всей предыдущей
+                    деятельности.
+                </div>
+                <div class="services__item-btn">
+                    <a class="services__item-link" href="#">Подробнее</a>
+                    <a data-fancybox data-src="#modal" href="javascript:;" class="default-btn">
+                        Заказать
+                    </a>
+                </div>
+            </div>
+            <div class="services__item">
+                <img src="img/about-2.png" alt="">
+                <div class="services__item-title">
+                    Адвокатское сопровождение
+                </div>
+                <div class="services__item-text">
+                    Мы гарантируем полное профессиональное обслуживание клиентов в заявленных сферах деятельности и
+                    соблюдение конфиденциальности.
+                </div>
+                <div class="services__item-btn">
+                    <a class="services__item-link" href="#">Подробнее</a>
+                    <a data-fancybox data-src="#modal" href="javascript:;" class="default-btn">
+                        Заказать
+                    </a>
+                </div>
+            </div>
+            <div class="services__item">
+                <img src="img/about-3.png" alt="">
+                <div class="services__item-title">
+                    Налоговый аудит
+                </div>
+                <div class="services__item-text">
+                    Налоговый аудит проводят перед проведением налоговых проверок; при оценке возможных налоговых
+                    рисков; при оптимизации налогооблажения.
+                </div>
+                <div class="services__item-btn">
+                    <a class="services__item-link" href="#">Подробнее</a>
+                    <a data-fancybox data-src="#modal" href="javascript:;" class="default-btn">
+                        Заказать
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="about">
+    <div class="container">
+        <div class="about__inner">
+            <div class="about__title">
+                О компании
+            </div>
+            <div class="about__text">
+                Компания специализируется на оказании услуг в сфере корпоративного права,
+                налогового консалтинга, представительства в судах, ликвидации и банкротства предприятий.
+                На сегодняшний день, коллектив компании объединяет высокопрофессиональных экспертов имеющих
+                многолетний опыт в сфере корпоративного права.
+            </div>
+            <a href="#" class="about__btn default-btn">
+                Узнать больше
+            </a>
+        </div>
+    </div>
+</section>
+
+<div id="modal">
+    <h2>Hello!</h2>
+    <p>You are awesome!</p>
+</div>
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+<script src="js/jquery.fancybox.min.js"></script>
+<script src="js/slick.min.js"></script>
+<script src="js/main.js"></script>
+</body>
+
+</html>
